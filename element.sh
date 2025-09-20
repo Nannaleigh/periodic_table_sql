e74b84c15ae659e7a2cc82e985adda9d279698e2
Nannaleigh <tatianna.martinez71@gmail.com>
2025-09-20 18:53:08 -0400

    fix: typos in result query

--- element.sh
+++ element.sh
@@ -19,16 +19,22 @@ else
 CONDITION="e.name ILIKE '$INPUT'"
 fi
 
-QUERY="SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celisus, p.boiling_point_celsius
+QUERY="SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius
 FROM elements e
 JOIN properties p USING(atomic_number)
 JOIN types t USING(type_id)
 WHERE $CONDITION;"
 
-RESULT=${$PSQL "$QUERY"}
+RESULT=$($PSQL "$QUERY")
 
 if [[ -z "$RESULT" ]] 
 
 then
 echo "I could not find that element in the database."
+exit 0
 fi
+
+IFS="|" read -r ATOMIC_NUM NAME SYMBOL TYPE MASS MELT BOIL <<< "$RESULT"
+
+
+echo "The element with atomic number $ATOMIC_NUM is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
