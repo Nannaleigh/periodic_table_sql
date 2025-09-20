b330c2c2cbebfde4734e7082e5d79c9a8f8412ad
Nannaleigh <tatianna.martinez71@gmail.com>
2025-09-20 18:42:51 -0400

    feat: added input checks

--- element.sh
+++ element.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+if [[ -z "$1"]];
+then 
+echo "Please provide an element as an argument."
+exit 0
+fi
+
+INPUT=$1
+
+if [[ $INPUT =~ ^[0-9]+$ ]]
+then
+CONDITION="e.atomic_number = $INPUT";
+elif [[ ${#INPUT} -le 2 ]]
+then 
+CONDITION="e.symbol ILIKE '$INPUT'";
+else
+CONDITION="e.name ILIKE '$INPUT'";
+fi
